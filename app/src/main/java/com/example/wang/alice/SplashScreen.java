f33a66eaf392bad4e5f3ba7443936306295fe514
“Kenny25775” <zhizhengliu@yahoo.comssh-keygen -t rsa -C zhizhengliu@yahoo.comgit config --global color.ui truegit config --global user.name Kenny25775git config --global user.email zhizhengliu@yahoo.com>
2018-03-03 23:12:31 -0800

    with good Splash screen

--- app/src/main/java/com/example/wang/alice/SplashScreen.java
+++ app/src/main/java/com/example/wang/alice/SplashScreen.java
@@ -1,8 +1,10 @@
 package com.example.wang.alice;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            //translucent status
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //translucent navigation
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+        }
 
         Thread myThread = new Thread() {
             @Override
