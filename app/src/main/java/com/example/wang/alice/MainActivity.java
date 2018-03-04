d91b9985da615291301d821fd562b7b0f242c1e1
“Kenny25775” <zhizhengliu@yahoo.comssh-keygen -t rsa -C zhizhengliu@yahoo.comgit config --global color.ui truegit config --global user.name Kenny25775git config --global user.email zhizhengliu@yahoo.com>
2018-03-03 21:39:14 -0800

    Splash

--- app/src/main/java/com/example/wang/alice/MainActivity.java
+++ app/src/main/java/com/example/wang/alice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.wang.alice;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
@@ -10,7 +11,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +20,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private final static int PERMISSION_RECORD_AUDIO = 7000;
+
 
     private Button speakBtn;
     private SpeechRecognizer mSpeechRecognizer;
@@ -31,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
         speakBtn = findViewById(R.id.speak_btn);
 
         mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
